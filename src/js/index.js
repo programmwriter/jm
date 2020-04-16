7bf47cf5ea8da22dbae6212663479938fb03978b
programmwriter <shportaklev@mail.ru>
2020-04-16 22:04:35 +0500

    prices swiper

--- src/js/index.js
+++ src/js/index.js
@@ -9,18 +9,20 @@ window.onload = function () {
     ".technics .slide-pocket__items"
   );
   const technics = document.querySelector(".slide-pocket.technics");
+  const prices = document.querySelector(".prices");
 
   const brendsMobile = document.querySelector(".brends-mobile");
   const technicsMobile = document.querySelector(".technics-mobile");
+  const pricesMobile = document.querySelector(".prices-mobile");
 
   const moreBtnOpen = "more-btn--open";
 
   const slidePocketExpanded = "slide-pocket--expanded";
   const slidePocketItemsExpanded = "slide-pocket__items--expanded";
 
-  const moreBtn = document.querySelectorAll(".more-btn");
+  const moreBtns = document.querySelectorAll(".more-btn");
 
-  moreBtn.forEach((btn) => {
+  moreBtns.forEach((btn) => {
     btn.addEventListener("click", function (event) {
       if (event.target.dataset.block === "slide") {
         if (event.target.classList.contains(moreBtnOpen)) {
@@ -55,9 +57,18 @@ window.onload = function () {
     });
   });
 
-  var swiper = new Swiper(".swiper-container", {
-    spaceBetween: 0,
-    width: 256,
+  var brendswiper = new Swiper(".brends-mobile .swiper-container", {
+    spaceBetween: 16,
+    width: 240,
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+  var technicsSwiper = new Swiper(".technics-mobile .swiper-container", {
+    spaceBetween: 16,
+    width: 240,
 
     pagination: {
       el: ".swiper-pagination",
@@ -65,16 +76,33 @@ window.onload = function () {
     },
   });
 
-  if (window.innerWidth < 350) {
-    console.log(window.innerWidth);
+  var pricesSwiper = new Swiper(".prices-mobile  .swiper-container", {
+    spaceBetween: 16,
+    width: 260,
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  if (window.innerWidth < 700) {
+    console.log(`if ${window.innerWidth}`);
     brends.classList.add("display-none");
     technics.classList.add("display-none");
+    prices.classList.add("display-none");
+
     brendsMobile.classList.remove("display-none");
     technicsMobile.classList.remove("display-none");
+    pricesMobile.classList.remove("display-none");
   } else {
+    console.log(`else ${window.innerWidth}`);
     brends.classList.remove("display-none");
     technics.classList.remove("display-none");
+    prices.classList.remove("display-none");
+
     brendsMobile.classList.add("display-none");
     technicsMobile.classList.add("display-none");
+    pricesMobile.classList.add("display-none");
   }
 };
